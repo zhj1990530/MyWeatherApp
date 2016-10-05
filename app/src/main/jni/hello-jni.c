00abe6d524a08614a2cbed0168ae935da05f3228
huajun zhang <zhj1990530@gmail.com>
2016-10-05 15:38:53 -0700

    File upload

---
+++ app/src/main/jni/hello-jni.c
@@ -0,0 +1,13 @@
+#include <jni.h>
+#include <stdio.h>
+#include <string.h>
+//#include "TestJNIPrimitive.h"
+
+JNIEXPORT jdouble JNICALL Java_com_example_hellojni_MainActivity_stringFromJNI
+        (JNIEnv *env, jobject thisObj, jint n1, jint n2) {
+    jdouble result;
+    //printf("In C, the numbers are %d and %d\n", n1, n2);
+    result = ((jdouble)n1 + n2) / 2.0;
+    // jint is mapped to int, jdouble is mapped to double
+    return result;
+}
